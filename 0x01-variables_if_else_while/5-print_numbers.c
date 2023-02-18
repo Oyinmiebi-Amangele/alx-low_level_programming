eebf675242251afbb02bfcbd6b0f3ad9e7091359
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-02-18 02:21:57 +0000

    printing base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all single digit numbers in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("0123456789");
+	return (0);
+}
