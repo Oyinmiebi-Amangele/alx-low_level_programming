b9dd89c756f9d194173fc98ebbc191c14d06f7b7
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-02-23 22:18:41 +0000

    printing numbers from 0-9 and excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function prints numbers from 0-9.
+ * excluding 2 and 4.
+ *
+ * Return: no returns.
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50 && a != 52)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
