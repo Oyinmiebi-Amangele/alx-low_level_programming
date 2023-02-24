cfea025730fff0a26a55e28942a3a0c247a55d6c
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-02-24 02:31:01 +0000

    printing diagonals n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints diagonals n times.
+ * @n: the number of times the diagonal should be printed.
+ * Return: no returns.
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
