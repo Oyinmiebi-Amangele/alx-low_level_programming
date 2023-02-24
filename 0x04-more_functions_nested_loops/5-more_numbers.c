52e0b667a5231a08b7353184a1ebfcbf3d00d2ba
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-02-24 02:05:25 +0000

    correction on the function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,10 @@ void more_numbers(void);
 		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
+			{
 				_putchar((b / 10) + 48);
-			_putchar((b % 10) + 48);
+				_putchar((b % 10) + 48);
+			}
 		}
 		_putchar('\n');
 		a++;
