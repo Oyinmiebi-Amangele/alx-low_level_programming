dfdcf13453259c99c1c9423589b682a96bba565f
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-02-24 03:06:38 +0000

    printing fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints FIZZ for each multiples of 3 and BUZZ
+ * for each multiples of 5 and FIZZBUZZ for both multiples
+ * for 3 and 5.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FIZZBUZZ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" FIZZ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" BUZZ");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
