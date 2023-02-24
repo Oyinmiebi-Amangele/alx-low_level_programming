576a70737994a1402638deefced84c2a41e2ccbe
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-02-24 02:00:43 +0000

    printing 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to print numbers 0-14 ten times.
+ *
+ * Return: no returns.
+ */
+
+void more_numbers(void);
+{
+	int a = 0, b;
+
+	while (a < 10)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
