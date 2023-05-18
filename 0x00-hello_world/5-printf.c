2b4ec4468afa65f21e37855595557f98b925ab05
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-05-18 23:33:50 +0000

    using printf to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - using printf to print out a string.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
