da2cfb5ad34e6c4b1cab55197888497f24ad501d
Amangele Oyinmiebi <miebi.amangele@gmail.com>
2023-05-19 02:05:22 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
-#include <string.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, msg, strlen(msg));
+	write(2, msg, 59);
 	return (1);
 }
