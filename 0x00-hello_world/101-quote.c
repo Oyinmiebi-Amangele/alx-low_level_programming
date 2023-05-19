88d33b1433d846687d676bb89a9583bc125b9ca3
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-05-19 01:02:29 +0000

    using write to print a string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	const char *msg = "and that piece of art is useful\" Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, strlen(msg));
+	return (1);
+}
+
