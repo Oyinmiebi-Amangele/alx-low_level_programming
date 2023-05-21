fb3af58d8621d13866c71b37d7d1d6a63a3010fa
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-05-21 23:27:38 +0000

    printing lower and uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - printing lower and uppercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i <= 122; ++i)
+	{
+		putchar(i);
+	}
+	for (j = 65; j <= 90; ++j)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
