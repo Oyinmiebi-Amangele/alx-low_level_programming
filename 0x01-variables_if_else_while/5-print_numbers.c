3b2a2d015269d1031304127a251e7ebd703eeb7a
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-05-22 12:13:38 +0000

    printing out base10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all singlt digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+	return (0);
+}
+
