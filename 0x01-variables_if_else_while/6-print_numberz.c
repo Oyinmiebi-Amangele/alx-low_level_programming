9be5533d9cc4694e15614da905d29b7564e8d24a
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-05-22 15:01:56 +0000

    program to print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - to print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
