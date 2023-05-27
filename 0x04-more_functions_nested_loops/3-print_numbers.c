4c3960d1de91d82e58796f15c417f2d6d3117592
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-05-27 01:37:40 +0000

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints number 0 - 9.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
