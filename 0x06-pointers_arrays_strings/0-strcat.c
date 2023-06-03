c180c26befa10f3c10f084800bfaa00905c735ad
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-06-03 01:18:25 +0000

    joining two strings together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - function that joins two strings together.
+ * @src: sorce string.
+ * @dest: destination string.
+ *
+ * Return: returns the destination string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		dest[count] = src[count2];
+		if (src[count2] == '\0')
+		{
+			break;
+		}
+		count++;
+		count2++;
+	}
+
+	return (dest);
+}
+
