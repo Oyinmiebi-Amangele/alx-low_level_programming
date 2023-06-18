d6c8d5320dba5f2c2d2e5b6aa0869d40300f04cb
Amangele Oyinmiebi <miebi.amangele@gmail.com>
2023-06-19 00:30:34 +0100

    function that creates a 2-dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,14 +12,13 @@
 
 int **alloc_grid(int width, int height)
 {
-	int gridout;
+	int **gridout;
 	int i, j;
 
 	if (width < 1 || height < 1)
 		return (NULL);
 
 	gridout = malloc(height * sizeof(int *));
-
 	if (gridout == NULL)
 	{
 		free(gridout);
@@ -32,9 +31,7 @@ int **alloc_grid(int width, int height)
 		if (gridout[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-			{
 				free(gridout[i]);
-			}
 			free(gridout);
 			return (NULL);
 		}
@@ -43,5 +40,6 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			gridout[i][j] = 0;
+
 	return (gridout);
 }
