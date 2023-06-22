d4ba3eeb812ed0f267ff447c4c14378057192fdc
Amangele Oyinmiebi <miebi.amangele@gmail.com>
2023-06-22 15:04:42 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,13 +13,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
+    if (array == NULL || cmp == NULL || size <= 0)
+        return (-1);
 
-	for (int i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
-	}
-	return (-1);
+    for (int i = 0; i < size; i++)
+    {
+        if (cmp(array[i]) != 0)
+            return (i);
+    }
+
+    return (-1);
 }
