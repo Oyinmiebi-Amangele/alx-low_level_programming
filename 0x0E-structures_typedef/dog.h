ba2af93fa587c8f9002ebe4f431f2563b5d22e1d
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-07-19 00:18:43 +0000

    a header for the struct of a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - stores the information about dogs.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ *
+ * Description: the struct called 'dog', stores its name
+ * the age and the owner ofthe dog.
+ */
+
 struct dog
 {
 	char *name;
